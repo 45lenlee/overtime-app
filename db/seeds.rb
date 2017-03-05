fe282a734ada5433369ba9a246d6dec910925c44
Len Lee <lenlee454@gmail.com>
2017-03-04 16:09:34 -0800

    add phone and custom rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin",
-                 last_name: "Name")
+                 last_name: "Name", phone: "4322386131")
 
 puts "1 Admin User created"
 
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf",
-                    first_name: "Jon", last_name: "Snow")
+                    first_name: "Jon", last_name: "Snow", phone: "4322386131")
 
 puts "1 User created"
 
